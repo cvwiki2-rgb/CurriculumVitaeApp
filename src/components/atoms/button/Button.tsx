2488f467ddac9f310429b2490715feca732ca967
Diana Selyuk <selukdiana@gmail.com>
2025-11-07 12:40:11 +0300

    feat: add ripple on btn on btn click

--- src/components/atoms/button/Button.tsx
+++ src/components/atoms/button/Button.tsx
@@ -1,12 +1,12 @@
+import { useRef, type ButtonHTMLAttributes, type ReactNode } from 'react';
 import classNames from 'classnames';
 import styles from './button.module.scss';
-import type { ButtonHTMLAttributes } from 'react';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: 'outlined' | 'contained' | 'text';
   color?: 'primary' | 'secondary';
   disabled?: boolean;
-  children: string;
+  children: ReactNode;
 }
 
 export const Button = ({
@@ -15,8 +15,29 @@ export const Button = ({
   disabled = false,
   children,
   className,
-  ...rest
 }: ButtonProps) => {
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const button = buttonRef.current;
+    if (!button) return;
+    const ripple = document.createElement('span');
+    ripple.className = styles.ripple;
+
+    const rect = button.getBoundingClientRect();
+    const size = Math.max(rect.width, rect.height);
+    const x = e.clientX - rect.left - size / 2;
+    const y = e.clientY - rect.top - size / 2;
+
+    ripple.style.width = ripple.style.height = `${size}px`;
+    ripple.style.left = `${x}px`;
+    ripple.style.top = `${y}px`;
+
+    button.appendChild(ripple);
+
+    setTimeout(() => ripple.remove(), 600);
+  };
+
   const buttonClassName = classNames(
     styles.button,
     styles[variant],
@@ -25,7 +46,13 @@ export const Button = ({
   );
 
   return (
-    <button className={buttonClassName} disabled={disabled} {...rest}>
+    <button
+      ref={buttonRef}
+      className={buttonClassName}
+      disabled={disabled}
+      type="button"
+      onClick={handleClick}
+    >
       {children}
     </button>
   );
