d7de0913eb279e260ccd7276b3ac20f51626c743
dzlek <6894255@gmail.com>
2025-11-12 12:22:31 +0300

    feat: add i18n internationalization

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { useTranslation } from 'react-i18next';
+
 function App() {
+  const { t, i18n } = useTranslation();
   return (
     <>
-      <h1>Started...</h1>
+      <h1>{t('Started')}...</h1>
+      <div>
+        <button onClick={() => i18n.changeLanguage('ru')}>RU</button>
+        <button onClick={() => i18n.changeLanguage('en')}>EN</button>
+      </div>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { theme } from './theme.ts';
 import './styles/index.scss';
+import './app/i18n/i18n.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
