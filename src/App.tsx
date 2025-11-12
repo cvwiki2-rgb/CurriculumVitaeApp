1670d03d8b88eb4eda112685cd8438f62ff48d1e
dzlek <6894255@gmail.com>
2025-11-12 12:22:31 +0300

    feat: add i18n internationalization

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import Switch from '@mui/material/Switch';
 import { useAppTheme } from './context/ThemeContext';
 
+import { useTranslation } from 'react-i18next';
+
 function App() {
   const { toggleTheme, mode } = useAppTheme();
 
+  const { t, i18n } = useTranslation();
   return (
     <>
       <Switch checked={mode === 'dark'} onChange={toggleTheme} />
-      <h1>Started...</h1>
+      <h1>{t('Started')}...</h1>
+      <div>
+        <button onClick={() => i18n.changeLanguage('ru')}>RU</button>
+        <button onClick={() => i18n.changeLanguage('en')}>EN</button>
+      </div>
     </>
   );
 }
---
+++ src/app/i18n/i18n.ts
@@ -0,0 +1,17 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import enTranslation from './locales/en/en.json';
+import ruTranslation from './locales/ru/ru.json';
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: { translation: enTranslation },
+    ru: { translation: ruTranslation },
+  },
+  lng: 'en', // default
+  fallbackLng: 'en',
+  debug: true,
+  interpolation: { escapeValue: false },
+});
+
+export default i18n;
