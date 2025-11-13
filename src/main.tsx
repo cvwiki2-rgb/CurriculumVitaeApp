70e694c4480971f79947fe367b5e6ac9c14134d0
Diana Selyuk <selukdiana@gmail.com>
2025-11-13 14:00:39 +0300

    feat: add router provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import { RouterProvider } from 'react-router/dom';
 import { ThemeProvider } from './context/ThemeContext.tsx';
+import { router } from './routes/index.tsx';
 import './styles/index.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </StrictMode>,
 );
