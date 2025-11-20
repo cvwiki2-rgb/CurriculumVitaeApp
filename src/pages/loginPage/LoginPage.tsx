53cccb1af34c55d53edd7d9aefca3c5329dee953
Diana Selyuk <selukdiana@gmail.com>
2025-11-20 16:20:43 +0300

    refactor: simplify LoginPage layout by integrating AuthPageLayout and optimizing styles

--- src/pages/loginPage/LoginPage.tsx
+++ src/pages/loginPage/LoginPage.tsx
@@ -1,71 +1,38 @@
-import { Box, FormControl, Typography } from '@mui/material';
+import { Box, FormControl } from '@mui/material';
 import { StyledButton } from '../../components/atoms/styledButton';
 import { StyledInput } from '../../components/atoms/styledInput';
 import { PasswordInput } from '../../components/molecules/passwordInput';
+import { AuthPageLayout } from '../../components/organisms/authPageLayout';
 
 export const LoginPage = () => {
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        maxWidth: 560,
-        width: ' 90%',
-        height: 'calc(100vh - 56px)',
-        margin: ' 0px auto',
-        '& .MuiTypography-h4': {
-          marginBottom: '24px',
-        },
-        '& .MuiTypography-body1': {
-          marginBottom: '40px',
-        },
-
-        '& .MuiTextField-root': {
-          marginBottom: '20px',
-        },
-      }}
-      component="form"
+    <AuthPageLayout
+      title="Welcome back"
+      subtitle="Hello again! Log in to continue"
     >
-      <Typography
-        variant="h4"
-        component="h4"
-        sx={{
-          textAlign: 'center',
-        }}
-      >
-        Welcome back
-      </Typography>
-      <Typography
-        variant="body1"
-        sx={{
-          textAlign: 'center',
-        }}
-      >
-        Hello again! Log in to continue
-      </Typography>
       <FormControl>
         <StyledInput
           variant="outlined"
           label="Email"
-          placeholder="expamle@mail.com"
-          sx={{
-            '& .MuiInputBase-root': {},
-            '& .MuiInputLabel-root': {},
-          }}
+          placeholder="example@mail.com"
+          autoComplete="email"
         ></StyledInput>
       </FormControl>
       <FormControl>
-        <PasswordInput label="Password" placeholder="Enter your password" />
+        <PasswordInput
+          label="Password"
+          placeholder="Enter your password"
+          autoComplete="current-password"
+        />
       </FormControl>
       <Box
-        sx={{
+        sx={(theme) => ({
           maxWidth: 220,
-          margin: '40px auto 0px',
+          margin: `${theme.spacing(5)} auto 0`,
           display: 'flex',
           flexDirection: 'column',
-          gap: '8px',
-        }}
+          gap: theme.spacing(1),
+        })}
       >
         <StyledButton color="primary" variant="contained">
           Log in
@@ -74,6 +41,6 @@ export const LoginPage = () => {
           Forgot password
         </StyledButton>
       </Box>
-    </Box>
+    </AuthPageLayout>
   );
 };
