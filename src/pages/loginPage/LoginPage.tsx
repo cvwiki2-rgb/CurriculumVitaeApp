0cd0176ffe29c4725303cc196a882929212d04f2
Diana Selyuk <selukdiana@gmail.com>
2025-11-22 18:58:52 +0300

    feat: implement login functionality in LoginPage with lazy query and error handling

--- src/pages/loginPage/LoginPage.tsx
+++ src/pages/loginPage/LoginPage.tsx
@@ -1,10 +1,15 @@
+import { useEffect } from 'react';
 import { Controller, useForm, type SubmitHandler } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
+import { useLazyQuery } from '@apollo/client/react';
 import { StyledButton } from '../../components/atoms/styledButton';
 import { StyledInput } from '../../components/atoms/styledInput';
 import { AuthActionsContainer } from '../../components/molecules/authActionsContainer';
 import { PasswordInput } from '../../components/molecules/passwordInput';
 import { AuthPageLayout } from '../../components/organisms/authPageLayout';
+import { LOGIN_QUERY } from '../../graphql/auth/queries';
+import { setAuth } from '../../graphql/state/auth';
+import type { AuthInput, AuthResult } from 'cv-graphql';
 
 type LoginFormValues = {
   email: string;
@@ -24,10 +29,38 @@ export const LoginPage = () => {
     },
   });
 
+  const [execLogin, { data, loading, error }] = useLazyQuery<
+    { login: AuthResult },
+    { auth: AuthInput }
+  >(LOGIN_QUERY, {
+    fetchPolicy: 'no-cache',
+  });
+
   const onSubmit: SubmitHandler<LoginFormValues> = (data) => {
     console.log('Form submitted', data);
-    // тут будет useLoginQuery
+    execLogin({
+      variables: { auth: { email: data.email, password: data.password } },
+    });
   };
+
+  useEffect(() => {
+    if (!data) return;
+
+    if (data.login) {
+      setAuth(data?.login);
+      // redirect /users
+    } else {
+      // setSnackbar({ open: true, message: t('Unexpected server response') });
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (!error) return;
+
+    // const msg =
+    //   error.graphQLErrors?.[0]?.message ?? error.message ?? t('Login failed');
+    // setSnackbar({ open: true, message: msg });
+  }, [error]);
 
   return (
     <AuthPageLayout
@@ -66,10 +99,15 @@ export const LoginPage = () => {
         )}
       />
       <AuthActionsContainer>
-        <StyledButton type="submit" color="primary" variant="contained">
+        <StyledButton
+          type="submit"
+          color="primary"
+          variant="contained"
+          disabled={loading}
+        >
           {t('Log in')}
         </StyledButton>
-        <StyledButton color="secondary" variant="text">
+        <StyledButton color="secondary" variant="text" disabled={loading}>
           {t('Forgot password')}
         </StyledButton>
       </AuthActionsContainer>
