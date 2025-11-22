68d711e466c81dd3cd4d1d21cc4ab3499b535c81
Diana Selyuk <selukdiana@gmail.com>
2025-11-22 19:17:13 +0300

    feat: wrap application in ApolloProvider for GraphQL integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react';
 import { I18nextProvider } from 'react-i18next';
+import { ApolloProvider } from '@apollo/client/react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router/dom';
 import i18n from './app/i18n/i18n.ts';
 import { ThemeProvider } from './context/ThemeContext.tsx';
+import { apolloClient } from './graphql/client.ts';
 import { router } from './routes/index.tsx';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -11,12 +13,16 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/600.css';
 import '@fontsource/roboto/700.css';
 
+//initAuthFromStorage();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <I18nextProvider i18n={i18n}>
-      <ThemeProvider>
-        <RouterProvider router={router} />
-      </ThemeProvider>
-    </I18nextProvider>
+    <ApolloProvider client={apolloClient}>
+      <I18nextProvider i18n={i18n}>
+        <ThemeProvider>
+          <RouterProvider router={router} />
+        </ThemeProvider>
+      </I18nextProvider>
+    </ApolloProvider>
   </StrictMode>,
 );
