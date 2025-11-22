7c1adc2f522469791aecf593d768ccd5af9cceef
Diana Selyuk <selukdiana@gmail.com>
2025-11-22 18:44:32 +0300

    feat: create LOGIN_QUERY for user authentication in GraphQL

---
+++ src/graphql/auth/queries.ts
@@ -0,0 +1,21 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_QUERY = gql`
+  query Login($auth: AuthInput!) {
+    login(auth: $auth) {
+      access_token
+      refresh_token
+      user {
+        id
+        email
+        role
+        is_verified
+        profile {
+          id
+          full_name
+          avatar
+        }
+      }
+    }
+  }
+`;
