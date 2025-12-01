f4a5ca3e50f9ff2453e3ee2da9bb0361ead3a8ee
Diana Selyuk <selukdiana@gmail.com>
2025-12-01 22:28:44 +0300

    feat: create SkillsList component for displaying a list of skills with a title and grid layout

---
+++ src/components/molecules/skillsList/SkillsList.tsx
@@ -0,0 +1,26 @@
+import type { ReactElement } from 'react';
+import { Box, Typography } from '@mui/material';
+import { SkillItem } from '../skillItem';
+
+export interface SkillsListProps {
+  title: string;
+  children: ReactElement<typeof SkillItem>[];
+}
+
+export const SkillsList = ({ title, children }: SkillsListProps) => {
+  return (
+    <Box component="div">
+      <Typography variant="body1">{title}</Typography>
+      <Box
+        component="div"
+        sx={{
+          marginTop: '16px',
+          display: 'grid',
+          gridTemplateColumns: '1fr 1fr 1fr',
+        }}
+      >
+        {children}
+      </Box>
+    </Box>
+  );
+};
